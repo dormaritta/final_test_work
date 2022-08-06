59a7c24322c7ae140f8abf9e76ffa65c2030e0cb
Dara Kosova <darakosova.gb@gmail.com>
2022-08-07 00:29:16 +0400

    Рабочая версия через ввод массива пользователем

--- task/Program.cs
+++ task/Program.cs
@@ -5,28 +5,38 @@
 либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.
+
 Примеры:
 
 ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 ["Russia", "Denmark", "Kazan"] -> [] */
 
-Console.WriteLine("Введите элементы для сортировки через запятую: ");
-string[] array = Console.ReadLine()!.Split(',');
+string[] FillArray()
+{
+    Console.WriteLine("Введите элементы для сортировки через пробел: ");
+    string[] array = Console.ReadLine()!.Split(" ");
+    return array;
+}
 
 void ArraySorting(string[] massif)
 {
-    int ElementSize = 4;
-    string[] SortedArray = new string[massif.Length];
+    int ElementSize = 3;
     var element = string.Empty;
+    string[] SortedArray = new string[massif.Length];
 
     for (int i = 0; i < massif.Length; i++)
     {
         element = massif[i];
-        if (element.Length < ElementSize)
+
+        if (element.Length <= ElementSize)
         {
             SortedArray[i] = massif[i];
         }
     }
-    Console.WriteLine($"Отсортированный массив: [{string.Join(",", massif)}]");
+    SortedArray = SortedArray.Where(x => x != null).ToArray();
+    Console.WriteLine($"Отсортированный массив: {string.Join(", ", SortedArray)}");
 }
+
+string[] result = FillArray();
+ArraySorting(result);
